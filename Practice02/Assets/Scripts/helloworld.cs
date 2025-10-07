52b4984f85ca4b61c7160db185e8363576c4c44a
nimbl9 <jonesnow67@gmail.com>
2025-10-07 12:12:33 +0500

    fixed cube jumping and added platform with collision

--- Practice02/Assets/Scripts/helloworld.cs
+++ Practice02/Assets/Scripts/helloworld.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class CubeJump : MonoBehaviour
 {
+    public float jumpForce = 5f; // Сила прыжка
+    private Rigidbody rb;
+
     void Start()
     {
-        Debug.Log("Hello, GameDev! In KAZAKHSTAN!");
+        // Получаем компонент Rigidbody
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Проверяем, нажата ли клавиша пробела
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Применяем силу вверх
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
